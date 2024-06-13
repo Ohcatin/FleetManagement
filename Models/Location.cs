8da62292ed58591bfe7cf11de741aa776d36dcb7
Ohcatin <catalinajaviera.ordenes@gmail.com>
2024-06-13 13:20:19 -0400

    Actualizacion codigo hasta historia de usuario 3

---
+++ Models/Location.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FleetManagement.Models
+{
+    public class Location
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public int TaxiId { get; set; }
+
+        [Required]
+        public double Latitude { get; set; }
+
+        [Required]
+        public double Longitude { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        [ForeignKey("TaxiId")]
+        public Taxi Taxi { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,11 +7,30 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Taxis API",
+        Version = "v1"
+    });
+});
+
 // Configure DbContext with PostgreSQL
 builder.Services.AddDbContext<TaxiContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// Configure CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
+});
 
 var app = builder.Build();
 
@@ -28,6 +47,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
+
+// Use CORS policy
+app.UseCors("AllowAll");
+
 app.MapControllers();
 app.Run();
 
